0ce6066237bee77f697e71eab1b21673f1eaf99c
Qasim Iqbal <me@qas.im>
2015-01-31 08:37:53 -0500

    Added match hobbies to site

--- routes/analyze.js
+++ routes/analyze.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var engine = require('../engine/engine')
 
 router.get('/', function(req, res, next) {
+
+  var interests = req.query['interests'];
+  var location = req.query['location'];
 
   req.getConnection(function (err, connection) {
     connection.query('SELECT * FROM hobbies', function (err, results) {
       if (err) return next(err);
-      res.json(results);
+      var data = engine.matchHobbies(interests, location);
+      res.json(data);
     });
   });
 
